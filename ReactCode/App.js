9418a04021c5ce68f7f86e18e1a2af38144baad1
sagar <sagar.goyal@synoriq.in>
2023-01-22 21:39:16 +0530

    thrid lecture

--- ReactCode/App.js
+++ ReactCode/App.js
@@ -1,25 +1,18 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "namaste react"
-);
+const Heading=()=>(
+  <h1>this is namaste react course</h1>
+)
 
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title2",
-  },
-  "namaste react"
-);
+//Component composition
+const ComponentCompositon=()=>{
+  return(
+    <div>
+      <Heading/>
+    </div>
+  )}
 
-const container = React.createElement("div", { id: "container" }, [
-  heading,
-  heading2,
-]);
+  
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<ComponentCompositon/>);
